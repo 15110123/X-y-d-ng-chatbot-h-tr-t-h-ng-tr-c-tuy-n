b18f7fc9f48b5e01ce0bc851d1cf883982183661
15110123 <leminhtanvatc@yahoo.com.vn>
2018-05-29 20:28:18 +0700

    done

--- CutieShop/CutieShop/Controllers/ReportController.cs
+++ CutieShop/CutieShop/Controllers/ReportController.cs
@@ -1,8 +1,13 @@
+using CutieShop.Models.DAOs;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using SelectPdf;
+using System.Collections.Generic;
+using System.Dynamic;
 using System.Net.Http;
 using System.Threading.Tasks;
+using static CutieShop.Models.Utils.HtmlConvertUtil;
 //using RazorPDFCore;
 
 namespace CutieShop.Controllers
@@ -10,26 +15,57 @@ namespace CutieShop.Controllers
     [Route("[controller]")]
     public class ReportController : Controller
     {
-        public async Task<IActionResult> index()
+        [Route("{month}")]
+        public async Task<IActionResult> index(int month)
         {
-            using (var httpClient = new HttpClient()){
-                var res = await (await httpClient.GetAsync("http://localhost:53730/api/reportapi/all")).Content.ReadAsStringAsync();
-                var obj = JsonConvert.DeserializeObject<dynamic>(res);
-                var IdReportNavigation_DateElWarehouse = obj[0].DateElWarehouse;
-                string IdJoin = obj.joinXX[1].idJoin;
-                string IdTdk = obj.joinXX[2].idTdk;
-                string IdNtk = obj.joinXX[3].idNtk;
-                string IdXtk = obj.joinXX[4].idXtk;
-                string IdTck = obj.joinXX[5].idTck;
+            using (var reportDAO = new ReportDAO())
+            {
+                dynamic expObj = new ExpandoObject();
 
-                var html = View(obj).ToHtml(HttpContext);
+                var foundReport = await reportDAO.Context.Report
+                .Include(x => x.JoinXx)
+                .ThenInclude(x => x.IdJoinNavigation)
+                .Include(x => x.JoinXx)
+                 .ThenInclude(x => x.IdTdkNavigation)
+                 .Include(x => x.JoinXx)
+                 .ThenInclude(x => x.IdNtkNavigation)
+                 .Include(x => x.JoinXx)
+                 .ThenInclude(x => x.IdXtkNavigation)
+                 .Include(x => x.JoinXx)
+                 .ThenInclude(x => x.IdTckNavigation)
+                .FirstOrDefaultAsync(x => x.DateElWarehouse.Value.Month == month);
+
+                expObj.DateElWarehouse = foundReport.DateElWarehouse.Value;
+
+                var lstReportRow = new List<dynamic>();
+
+                foreach (var ele in foundReport.JoinXx)
+                {
+                    dynamic expEle = new ExpandoObject();
+                    expEle.IdProduct = ele.IdJoinNavigation.IdProduct;
+                    expEle.ProductName = ele.IdJoinNavigation.ProductName;
+                    expEle.Unit = ele.IdJoinNavigation.Unit;
+                    expEle.QuantityTdk = ele.IdTdkNavigation.QuantityTdk;
+                    expEle.Total1 = ele.IdTdkNavigation.Total;
+                    expEle.QuantityNtk = ele.IdNtkNavigation.QuantityNtk;
+                    expEle.Total2 = ele.IdNtkNavigation.Total;
+                    expEle.QuantityXtk = ele.IdXtkNavigation.QuantityXtk;
+                    expEle.Total3 = ele.IdXtkNavigation.Total;
+                    expEle.QuantityTck = ele.IdTckNavigation.QuantityTck;
+                    expEle.Total4 = ele.IdXtkNavigation.Total;
+                    lstReportRow.Add(expEle);
+                }
+
+                expObj.ReportRow = lstReportRow;
+
+                var html = (View(expObj) as ViewResult).ToHtml(HttpContext);
                 var converter = new HtmlToPdf();
                 var doc = converter.ConvertHtmlString(html);
                 var pdfByte = doc.Save();
                 doc.Close();
-            return File(pdfByte, "application/pdf");
-            //return ViewPdf(null, "report.pdf", "index", false);
-                        }
+                return File(pdfByte, "application/pdf");
+            }
         }
     }
 }
+
